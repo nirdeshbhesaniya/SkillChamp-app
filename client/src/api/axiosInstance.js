3e285d0476aec415ff3038d1e241fda68b98e542
Bhesaniya Nirdesh Parsotambhai <147792857+nirdeshbhesaniya@users.noreply.github.com>
2025-03-12 15:43:06 +0530

    Update axiosInstance.js
    http://localhost:5000

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://skill-champ-app.vercel.app",
 });
 
 axiosInstance.interceptors.request.use(
