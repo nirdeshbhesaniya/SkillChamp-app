379d97421084b699c376d049b854a4a1f4845b59
Bhesaniya Nirdesh Parsotambhai <147792857+nirdeshbhesaniya@users.noreply.github.com>
2025-03-12 16:53:19 +0530

    Update server.js
    set core origine from vercel url to local host

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 app.use(
   cors({
-    origin: "https://skill-champ.vercel.app",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
